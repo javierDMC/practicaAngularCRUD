2f413820a0ec46aea2608761201f178113ad17ef
javierdelmoralcamacho@gmail.com <javierdelmoralcamacho@gmail.com>
2023-01-25 09:43:44 +0100

    Componentes clientes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClienteComponent } from './components/clientes/cliente/cliente.component';
+import { ClientesComponent } from './components/clientes/clientes/clientes.component';
+import { ClienteNuevoComponent } from './components/clientes/cliente-nuevo/cliente-nuevo.component';
+import { ClienteModificarComponent } from './components/clientes/cliente-modificar/cliente-modificar.component';
+import { ClienteBorrarComponent } from './components/clientes/cliente-borrar/cliente-borrar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClienteComponent,
+    ClientesComponent,
+    ClienteNuevoComponent,
+    ClienteModificarComponent,
+    ClienteBorrarComponent
   ],
   imports: [
     BrowserModule,
